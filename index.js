c07769c0e6daeeb4c5f52d997c766469d1ad6acf
Charan0413 <charanyadav417@gmail.com>
2025-06-02 19:38:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+app.listen(8080, () =>{
+    console.log("Server Started")
+})
+
+app.get("/",(req,res)=>{
+    return res.send("hello world")
+})
+app.get("/greet",(req,res)=>{
+   return res.send (("Greetinngs"))
+})
+app.get("/name",(req,res)=> {res.send("Sricharan")})
+app.get("/weather",(req,res)=> {res.send("31Degrees")})
