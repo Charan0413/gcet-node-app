fba92ba74fb53cd2ef0edcb6abf420de9de276c8
Charan0413 <charanyadav417@gmail.com>
2025-06-03 12:28:26 +0530

    updated

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 import express from 'express'
+import mongoose, { mongo } from 'mongoose'
+import cors from "cors"
 const app = express()
 app.listen(8080, () =>{
+    mongoose.connect("mongodb://localhost:27017/gcet ")
     console.log("Server Started")
-})
+});
 
-app.get("/",(req,res)=>{
-    return res.send("hello world")
-})
-app.get("/greet",(req,res)=>{
-   return res.send (("Greetinngs"))
-})
-app.get("/name",(req,res)=> {res.send("Sricharan")})
-app.get("/weather",(req,res)=> {res.send("31Degrees")}) 
-app.get("/morning",(req,res)=> {res.send("Good morniing!!")}) 
+const userSchema =mongoose.Schema({
+    name: {type: String},
+    email: {type: String},
+    pass: {type: String}
+   
+});
+
+const user = mongoose.model("user", userSchema);
+app.use(cors());
+app.use(express.json())
+
+app.post("/register", async (req, res) => {
+    const {name,email,pass} = req.body
+    const result = await user.insertOne({name,email,pass});
+    return res.json(result);
+});
+
+const loginSchema =mongoose.Schema({
+    name: {type: String},
+    email: {type: String},
+    pass: {type: String}
+   
+});
+const login = mongoose.model("login", loginSchema);
+app.use(cors());
+app.use(express.json())
+
+app.post("/login", async (req, res) => {
+    const {email,pass} = req.body
+    const result = await user.insertOne({email,pass});
+    return res.json(result);
+});
+// app.get("/",(req,res)=>{
+//     return res.send("hello world")
+// })
+// app.get("/greet",(req,res)=>{
+//    return res.send (("Greetinngs"))
+// })
+// app.get("/name",(req,res)=> {res.send("Sricharan")})
+// app.get("/weather",(req,res)=> {res.send("31Degrees")}) 
+// app.get("/morning",(req,res)=> {res.send("Good morniing!!")}) 
 
 
 
